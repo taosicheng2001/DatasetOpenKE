074730c7f429dd1216bab01779eb344a87b3f0ac
Tao SiCheng <tsc2001@mail.ustc.edu.cn>
2021-12-02 13:31:24 +0800

    ADD bash script

--- config_and_run.sh
+++ config_and_run.sh
@@ -1,7 +1,7 @@
-!git clone https://github.com/thunlp/OpenKE.git
-!cd OpenKE && cd openke && bash make.sh
-!git clone https://github.com/tusenchun/DatasetOpenKE.git
-!cp DatasetOpenKE -r OpenKE/
-!cd OpenKE && cp DatasetOpenKE/transh_dataset.py ./ 
-!cd OpenKE && mkdir checkopint && ls
-!python3 transh_dataset
+git clone https://github.com/thunlp/OpenKE.git
+cd OpenKE && cd openke && bash make.sh
+git clone https://github.com/tusenchun/DatasetOpenKE.git
+cp DatasetOpenKE -r OpenKE/
+cd OpenKE && cp DatasetOpenKE/transh_dataset.py ./ 
+cd OpenKE && mkdir checkopint && ls
+python3 transh_dataset
