58ea5ca0115a440844acca1a82fa96b2a6239001
Tao SiCheng <tsc2001@mail.ustc.edu.cn>
2021-12-04 09:59:23 +0800

    Update test

--- transh_dateset_test.py
+++ transh_dateset_test.py
@@ -9,21 +9,9 @@ from openke.data import TrainDataLoader, TestDataLoader
 # dataloader for test
 test_dataloader = TestDataLoader("./DatasetOpenKE/", "link")
 
-# dataloader for training
-train_dataloader = TrainDataLoader(
-	in_path = "./DatasetOpenKE/", 
-	nbatches = 100,
-	threads = 8, 
-	sampling_mode = "normal", 
-	bern_flag = 1, 
-	filter_flag = 1, 
-	neg_ent = 25,
-	neg_rel = 0)
 
 # define the model
 transh = TransH(
-	ent_tot = train_dataloader.get_ent_tot(),
-	rel_tot = train_dataloader.get_rel_tot(),
 	dim = 200, 
 	p_norm = 1, 
 	norm_flag = True)
